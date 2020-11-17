2d006e8af729656b2b2eedfdd14fd03180ad9627
Jyothik1234 <74532749+Jyothik1234@users.noreply.github.com>
2020-11-17 23:41:22 +1100

    uploaded

---
+++ Maven1/src/main/java/Additionfloat/AddFloat.java
@@ -0,0 +1,22 @@
+package Additionfloat;
+
+import java.util.Scanner;
+
+public class AddFloat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 {
+				Scanner s=new Scanner (System.in);
+				System.out.println("Enter the first float number");
+				float a=s.nextFloat();
+				System.out.println("Enter the second float number");
+				float b=s.nextFloat();
+		     	float c=a+b;;
+				System.out.print("sum of two  float number ="+c);
+
+
+	}
+
+	}
+}
